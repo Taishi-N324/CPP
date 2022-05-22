684a999b3dbdfda25bdc54c416de72ff777cccf6
Taishi <82321333+Taishi-N324@users.noreply.github.com>
2022-05-22 17:04:57 +0900

    Delete mySite directory

--- mySite/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
- 
-urlpatterns = [
-    path('templates/', views.index_template, name='index_template'),
-    #path('input', views.input),
-    path('templates/output', views.output),
-]
-
