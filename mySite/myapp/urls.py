1336b223872cf2acf3ab8b55fe82d1e6023b7e1c
Taishi <82321333+Taishi-N324@users.noreply.github.com>
2022-05-26 02:17:08 +0900

    Add files via upload

--- mySite/myapp/urls.py
+++ mySite/myapp/urls.py
@@ -7,7 +7,12 @@ urlpatterns = [
     path('templates/', views.input, name='input'),
     path('templates/output', views.output,name='output'),
     path('templates/', views.input_google, name='input_google'),
-    path('templates/google_translate', views.output_google,name='output_google')
+    path('templates/google_translate', views.output_google,name='output_google'),
+    path('templates/bleu', views.output_bleu,name='output_bleu'),
+    path('templates/output_test', views.output_test,name='output_test'),
+    path('templates/output_julius', views.output_julius,name='output_julius'),
+    path('templates/', views.input_julius,name='input_julius')
+    
     #path('templates/my_translate', views.input_my_translate,name='input_my_translate'),
     #path('templates/my_translate', views.output_my_translate,name='output_my_translate')
 ]
--- mySite/myapp/views.py
+++ mySite/myapp/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.template import loader
 from datetime import datetime as dt
 from django.http import HttpResponse
-
-
 import MeCab
 import ipadic
+import nltk
+import socket
+import time
+import re
 from transformers import pipeline
 from transformers import AutoModelForSequenceClassification
 from transformers import BertJapaneseTokenizer
@@ -34,6 +36,38 @@ def input(request):
 
 
 
+
+def input_bleu(request):
+    template = loader.get_template("index.html")
+    return HttpResponse(template.render({}, request))
+
+
+def output_bleu(request):
+    bleu_ref = request.POST["bleu_ref"]
+    bleu_text = request.POST["bleu_text"]
+    
+    reference = bleu_ref.split()
+    hypothesis= bleu_text.split()
+    references = [reference] 
+    list_of_references = [references]
+    list_of_hypotheses = [hypothesis]
+
+    result = nltk.translate.bleu_score.corpus_bleu(list_of_references, list_of_hypotheses)
+    result = result * 100
+
+
+    template = loader.get_template("bleu/index.html")
+    
+    context = {
+        "bleu_ref": bleu_ref,
+        "bleu_text": bleu_text,
+        "result": result,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+
+
 def input_google(request):
     template = loader.get_template("index.html")
     return HttpResponse(template.render({}, request))
@@ -56,13 +90,23 @@ def input_google(request):
 
 
 
+#def julius(request)
+
+
+
+
+
 def output_google(request):
     src = request.POST["google_translate"]
-    tr = Translator(service_urls=['translate.googleapis.com'])
-    trans = tr.translate(src, dest="en").text
+    try:
+        tr = Translator(service_urls=['translate.googleapis.com'])
+        trans = tr.translate(src, dest="en").text
+    except:
+        trans ="一日のAPIの呼び出し回数が限度を超えました"
+        
     template = loader.get_template("google_translate/index.html")
-    #trans ="Hello"
-    #src="aaaaa"
+  
+    
     context = {
         "input_date": trans,
         "original_date": src,
@@ -80,5 +124,78 @@ def output(request):
     context = {
         "input_date": input_date_new,
     }
+
+    return HttpResponse(template.render(context, request))
+
+
+def input_julius(request):
+    template = loader.get_template("index.html")
+    return HttpResponse(template.render({}, request))
+
+
+def output_julius(request):
+    result_julius = []
+    src = request.POST["output_julius"]
+
+    # ローカル環境のIPアドレス
+    #ここだけ変更を行う。
+    host = '192.168.56.1'
+
+    # Juliusとの通信用ポート番号
+    port = 10500
+
+    # Juliusにソケット通信で接続
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+    time.sleep(2)
+
+    # 正規表現で認識された言葉を抽出
+    extracted_word = re.compile('WORD="([^"]+)"')
+    data = ""
+
+    try:
+        while True:
+            while (data.find("</RECOGOUT>\n.") == -1):
+                data += str(client.recv(1024).decode('shift_jis'))
+
+            # 単語を抽出
+            recog_text = ""
+            for word in filter(bool, extracted_word.findall(data)):
+                recog_text += word
+
+            # 単語を表示
+            print("認識結果: " + recog_text)
+            result_julius.append(recog_text)
+            data = ""
+            if len(result_julius) == 5:
+                break
+
+    except:
+        print('PROCESS END')
+        client.send("DIE".encode('shift_jis'))
+        client.close()
+        
+    template = loader.get_template("output_julius/index.html")
+    
+    if len(result_julius)>0:
+        context = {'lists': result_julius}
+    else:
+        context = {'lists': ["音声を認識できませんでした。"]}
+        
+
+    return HttpResponse(template.render(context, request))
+
+
+def index_test(request):
+    context = {'lists': ["データ1", "データ2", "データ3"]}
+    return render(request, 'output_test/index.html', context)  
+
+
+
+def output_test(request):
+    
+    template = loader.get_template("output_test/index.html")
+    context = {'lists': ["データ1", "データ2", "データ3"]}
+
 
     return HttpResponse(template.render(context, request))
